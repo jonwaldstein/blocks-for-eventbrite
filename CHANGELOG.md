de332e7e81476afb33cfce18b991feab78f3c0bf
Jon Waldstein <jpwaldstein@gmail.com>
2020-05-06 15:10:46 -0400

    chore: update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 05-06-2020
+
+### Changed
+
+-   fixed conditionally display venue details to prevent event from not displaying
+
 ## [1.0.0] - 04-04-2020
 
 ### Added
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f4937e0aa7ef0b4a017f589caa46cd33');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4317e56b63f7a08c9460581671e847af');
