4e3e4ec7ea613d2569d9572ca7bd1a8aca6b19ec
Jon Waldstein <Jpwaldstein@gmail.com>
2020-06-22 12:23:49 -0400

    chore: update plugin version

--- blocks-for-eventbrite.php
+++ blocks-for-eventbrite.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Blocks for Eventbrite
  * Description:     Gutenberg blocks that display eventbrite events
- * Version:         1.0.2
+ * Version:         1.0.3
  * Author:          Jon Waldstein
  * Author URI:      https://jonwaldstein.com
  * License:         GPL-2.0-or-later
