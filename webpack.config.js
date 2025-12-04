ea5333428eb1a77db86cadb37f037426614985b9
Jon Waldstein <jonwaldstein@macbookair.mynetworksettings.com>
2025-12-04 07:51:33 -0500

    chore: update dev deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
+// Filter out the default CSS rules from @wordpress/scripts
+const filteredRules = defaultConfig.module.rules.filter( ( rule ) => {
+	// Keep rules that don't match CSS files
+	if ( ! rule.test ) {
+		return true;
+	}
+	const testString = rule.test.toString();
+	// Remove default CSS/SCSS rules (we'll add our own)
+	if ( testString.includes( '\\.css' ) || testString.includes( '\\.s[ac]ss' ) ) {
+		return false;
+	}
+	return true;
+} );
+
 module.exports = {
 	...defaultConfig,
 	module: {
 		...defaultConfig.module,
 		rules: [
-			...defaultConfig.module.rules,
+			...filteredRules,
+			// Non-module CSS (including tippy.js, base.css, etc.)
 			{
 				test: /^(?!.*?\.module).*\.css$/,
 				use: [
@@ -25,6 +40,7 @@ module.exports = {
 					},
 				],
 			},
+			// CSS Modules
 			{
 				test: /\.module\.css$/,
 				use: [
@@ -34,7 +50,10 @@ module.exports = {
 					{
 						loader: 'css-loader',
 						options: {
-							modules: true,
+							importLoaders: 1,
+							modules: {
+								localIdentName: '[local]',
+							},
 						},
 					},
 					{
